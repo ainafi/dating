fe2a9ecc015145e29ed151dcd3aedbbe723ae9ed
ainafi <razanaminoainafinaritra@gmail.com>
2024-10-14 11:21:55 +0300

    btn ajust 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import MultiStepForm from './MultiStep'
-import { Footer } from 'react-day-picker'
-
 const App:React.FC = () => {
   
 
   return (
     <div className='relative'>
         <MultiStepForm/>
-        <Footer/>
     </div>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-
-const Footer: React.FC = () => {
+const Footer = () => {
   return (
-   <footer className='fixed bottom-0 w-full' >
+    <footer className='fixed bottom-0 left-0 w-full'>
       <div className='flex items-center justify-center'>
         <p>&copy; 2024 - All rights reserved</p>
-        <p>power by <a href="https://github.com/ainafi" className='font-semibold text-blue-700'>ainafi</a></p>
+        <p>powered by <a href="https://github.com/ainafi" className='font-semibold text-blue-700'>ainafi</a></p>
       </div>
-   </footer>
-  )
+    </footer>
+  );
 }
 
 export default Footer
